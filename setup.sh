6dcc6ddbf3b4ea283893ad5ea96fa080aa6fb461
Isabel Arca <petitesabelle@hotmail.com>
2026-05-28 19:03:51 +0200

    Añado script de configuracion de hosts

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "127.0.0.1 lovestonedgirls.daw" | sudo tee -a /etc/hosts
+echo "127.0.0.1 admin.lovestonedgirls.daw" | sudo tee -a /etc/hosts
+echo "127.0.0.1 api.lovestonedgirls.daw" | sudo tee -a /etc/hosts
+echo "Dominios configurados correctamente"
