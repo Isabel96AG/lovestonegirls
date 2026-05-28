f05e727f3db9b001fe3ba5a9d0d795152f2b90ca
Isabel Arca <petitesabelle@hotmail.com>
2026-05-28 12:20:15 +0200

    Creacion de productod, arreglo de home

--- ecommerce/src/app/shared/footer/footer.component.ts
+++ ecommerce/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  email: string = '';
+  mensajeNewsletter: string = '';
+  tipoMensaje: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  suscribirse() {
+    if (!this.email || !this.email.includes('@')) {
+      this.mensajeNewsletter = 'Introduce un email válido';
+      this.tipoMensaje = 'error';
+      return;
+    }
+
+    this.http.post(`${URL_SERVICIOS}/ecommerce/newsletter/subscribe`, { email: this.email }).subscribe({
+      next: () => {
+        this.mensajeNewsletter = '¡Suscrito correctamente!';
+        this.tipoMensaje = 'success';
+        this.email = '';
+      },
+      error: () => {
+        this.mensajeNewsletter = 'No se pudo completar la suscripción';
+        this.tipoMensaje = 'error';
+      }
+    });
+  }
 }
